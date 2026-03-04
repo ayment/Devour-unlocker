cce9c389a667f9d491d5c595b9914815dfa60728
ayment <93206027+ayment@users.noreply.github.com>
2026-03-04 06:55:02 +0300

    Update sdk.h

--- Devour-unlocker/sdk.h
+++ Devour-unlocker/sdk.h
@@ -13,5 +13,9 @@ namespace Offsets
 {
 	uintptr_t UnlockedRop = 0x0;
 	uintptr_t UnlockedCharacters = 0x0;
+	uintptr_t Menu_SetupPerk = 0x0;
+	uintptr_t Menu_SetupOutfit = 0x0;
+	uintptr_t Menu_SetupFlashlight = 0x0;
+	uintptr_t Menu_SetupEmote = 0x0;
+	uintptr_t Menu_SetupPet = 0x0;
 }
-
